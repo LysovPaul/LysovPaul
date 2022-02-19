af3c2f580c88c77bdb377fb8c474a815910a8bb0
LysovPaul <91269844+LysovPaul@users.noreply.github.com>
2022-02-19 20:35:20 +0300

    add dependency

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,8 @@
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
