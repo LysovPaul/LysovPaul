08f93f35f60a198896765153fd08b13f2b86c9d8
pavel lysov <gerson90@mail.ru>
2022-03-22 20:13:57 +0300

    final 2.0

--- src/test/java/NolinkTest.java
+++ src/test/java/NolinkTest.java
@@ -2,6 +2,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.junit.AfterClass;
 
@@ -28,14 +29,13 @@ public class NolinkTest {
         driver.get(AppProperties.getProperty("page"));
     }
     /**
-     * метод получения номера старницы без ссылки путем отбрасывания текста адреса, кроме номера страницы
+     * метод получения номера старницы без видимой ссылки путем отбрасывания текста адреса, кроме номера страницы
      */
     public String getPageNum(String url) {
         int breaker = url.lastIndexOf('/');
         int point = url.lastIndexOf('.');
         return url.substring(breaker + 1, point);
     }
-
     /**
      * метод для поиска страниц без ссылок
      */
@@ -43,14 +43,34 @@ public class NolinkTest {
     public void nolinkTest() throws Exception {
         //стек для перебора страниц
         Stack<String> toVisit = new Stack<>();
+        //в качестве точки входа первая страница с ссылкой
         toVisit.push(page.getLinkBtn().getAttribute("href"));
         //множество для хранения страниц без ссылок
         Set<String> pagesNoLinks = new HashSet<String>();
-        //цикл перебирает страницы и складывает в множество только те, что без ссылок
+        //Начинаем перебирать страницы, пока есть ссылки
         while (!toVisit.isEmpty()) {
             driver.navigate().to(toVisit.pop());
-            // может быть несколько ссылок, проверяем каждую
-            driver.findElements(By.tagName("a")).forEach((elem) -> {
+            //Если увидели последнюю, останавливаемся
+            if (driver.findElement(By.tagName("body")).getText().contains("последняя страница"))
+                break;
+            List<WebElement> elems = driver.findElements(By.tagName("a"));
+            //когда ссылки нет даже скрытой, собираем страницу из текущей, добавляя в номер +1, чтобы перейти к следующей
+            if (elems.isEmpty()) {
+                String url = driver.getCurrentUrl();
+                int breaker = url.lastIndexOf('/');
+                int point = url.lastIndexOf('.');
+                String currPage = url.substring(breaker + 1, point);
+                pagesNoLinks.add(currPage);
+                String newUrl = String.format(
+                        "%s%d%s",
+                        url.substring(0, breaker + 1),
+                        Integer.parseInt(currPage) + 1,
+                        url.substring(point));
+                toVisit.push(newUrl);
+                continue;
+            }
+            elems.forEach((elem) -> {
+                //тут добавляем страницы, где ссылка есть, но не отображается. Возможно такие страницы не нужно учитывать, но я не подумал об этом сразу и не успел уточнил
                 if (!elem.isDisplayed())
                     pagesNoLinks.add(getPageNum(driver.getCurrentUrl()));
                 toVisit.push(elem.getAttribute("href"));
@@ -61,7 +81,7 @@ public class NolinkTest {
     /**
      * закрытие окна браузера
      */
-    //@AfterClass
+    @AfterClass
     public static void tearDown() {
         driver.quit();
     }
